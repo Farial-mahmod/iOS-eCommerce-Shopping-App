51396e9a851813d20eef1d5df35a734ab4f2a4c3
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-10-11 22:26:12 +0600

    Updating CartManager.swift with adding & deleting functionality

--- eCommerce Shopping App/Cart/CartManager.swift
+++ eCommerce Shopping App/Cart/CartManager.swift
@@ -1,5 +1,24 @@
 import Foundation
 
 class CartManager: ObservableObject{
+
+  @Published private(set) var products: [Product] = []
+  @Published private(set) var total: Float = 0
+
+  
+  // to add products
+  func addProduct(product: Product){
+    products.append(product)
+    total += product.price
+  }
+
+  // to remove products
+  removeProduct(product: Product){
+    // to remove a product from this array, the method .remove(at: ) could be an alternative
+    products = products.filter { $0.id != product.id }
+    total -= product.price
+  }
+
+  
   
 }
