74621707cea2ecf8e66b6d2435c9615b1b7f9c05
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-10-12 16:29:09 +0600

    Updating ContentView.swift

--- eCommerce Shopping App/View/ContentView.swift
+++ eCommerce Shopping App/View/ContentView.swift
@@ -1,2 +1,35 @@
-import Foundation
 import SwiftUI
+
+struct ContentView: View {
+    @StateObject var cart = CartManager()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 160), spacing: 20)], spacing: 20) {
+                    ForEach(productList, id: \.id) { product in
+                        ProductCard(product: product)
+                            .environmentObject(cart)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(Text("Shopping App"))
+            .toolbar {
+                NavigationLink {
+                    CartView()
+                        .environmentObject(cart)
+                } label: {
+                    CartButton(numberOfProducts: cart.products.count)
+                }
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
