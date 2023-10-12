a4d37c5f07da4e0c7a42a1ea7d19857ea14017f3
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-10-12 15:26:26 +0600

    adding CartButton.swift containing cart button view

---
+++ eCommerce Shopping App/Components/CartButton.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+// view of the cart button
+struct CartButton: View {
+    var numberOfProducts: Int
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            Image(systemName: "cart")
+                .padding(.top, 5)
+
+          // when products have been added to the cart i.e. products > 0
+            if numberOfProducts > 0 {
+                Text("\(numberOfProducts)")
+                    .font(.caption2).bold()
+                    .foregroundColor(.white)
+                    .frame(width: 15, height: 15)
+                    .background(Color(hue: 1.0, saturation: 0.89, brightness: 0.835))
+                    .cornerRadius(50)
+            }
+        }
+    }
+}
+
+struct CartButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CartButton(numberOfProducts: 1)
+    }
+}
